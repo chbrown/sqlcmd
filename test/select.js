cde835a826ff1166dab610c5633f4d2f6c2bdbc3
Christopher Brown <io@henrian.com>
2015-01-07 13:15:24 -0600

    Add a few test cases for more select command statement structures

--- test/select.js
+++ test/select.js
@@ -28,3 +28,26 @@ describe('db.Select(...).where(...)', function() {
     assert.deepEqual(command.parameters, {'1': 'Chris'});
   });
 });
+
+describe('db.Select(...).limit(...).orderBy(...)', function() {
+  var command = db.Select('users').limit(1).orderBy('lastname ASC', 'firstname ASC');
+  it('should equal literal string', function() {
+    assert.equal(command.toSQL(), 'SELECT * FROM users ORDER BY lastname ASC, firstname ASC LIMIT $limit');
+    assert.deepEqual(command.parameters, {limit: 1});
+  });
+});
+
+describe('db.Select(...).groupBy(...)', function() {
+  var command = db.Select('users').add('COUNT(lastname)').groupBy('lastname');
+  it('should equal literal string', function() {
+    assert.equal(command.toSQL(), 'SELECT COUNT(lastname) FROM users GROUP BY lastname');
+  });
+});
+
+describe('db.Select(...).offset(...).limit(...)', function() {
+  var command = db.Select('users').offset(250).limit(25);
+  it('should equal literal string', function() {
+    assert.equal(command.toSQL(), 'SELECT * FROM users LIMIT $limit OFFSET $offset');
+    assert.deepEqual(command.parameters, {offset: 250, limit: 25});
+  });
+});
