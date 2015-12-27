88cc5fe7e6b1e089587bd7b0e9903dd697468e76
Christopher Brown <io@henrian.com>
2015-12-27 17:25:53 -0600

    Change CreateTable#add() signature from single array to rest parameter

--- commands/CreateTable.ts
+++ commands/CreateTable.ts
@@ -17,13 +17,15 @@ export default class CreateTable extends Command<any[]> {
     parts.push(this.statement.table, '(', this.statement.columns.join(', '), ')');
     return parts.join(' ');
   }
-  _add(columns: string[]) {
+
+  _add(...columns: string[]) {
     this.statement.columns.push(...columns);
     return this;
   }
-  add(columns: string[]) {
-    return this.clone()._add(columns);
+  add(...columns: string[]) {
+    return this.clone()._add(...columns);
   }
+
   _ifNotExists() {
     this.statement.if_not_exists = true;
     return this;
